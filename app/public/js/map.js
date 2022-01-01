ff5c94b90d3cad5c45799b2c7c8c1ae6ec894978
John Watson <johnwatson484@hotmail.com>
2022-01-01 14:14:06 +0000

    Rotate aircraft

--- app/public/js/map.js
+++ app/public/js/map.js
@@ -1,24 +1,25 @@
-const image = new ol.style.Icon({
-  anchor: [15, 5],
-  anchorXUnits: 'pixels',
-  anchorYUnits: 'pixels',
-  src: 'assets/aircraft.png'
-})
-
-const styles = {
-  Point: new ol.style.Style({
-    image: image
-  }),
-  LineString: new ol.style.Style({
-    stroke: new ol.style.Stroke({ color: 'red', width: 5 })
-  })
-}
-
-const styleFunction = (feature) => {
-  return styles[feature.getGeometry().getType()]
-}
-
 const displayMap = (route) => { // eslint-disable-line no-unused-vars
+  const image = new ol.style.Icon({
+    anchor: [15, 5],
+    anchorXUnits: 'pixels',
+    anchorYUnits: 'pixels',
+    src: 'assets/aircraft.png',
+    rotation: route.rotation
+  })
+
+  const styles = {
+    Point: new ol.style.Style({
+      image: image
+    }),
+    LineString: new ol.style.Style({
+      stroke: new ol.style.Stroke({ color: 'red', width: 5 })
+    })
+  }
+
+  const styleFunction = (feature) => {
+    return styles[feature.getGeometry().getType()]
+  }
+
   const features = new ol.format.GeoJSON().readFeatures(route.geoJson)
   const source = new ol.source.Vector()
   features.forEach(feature => {
--- app/route.js
+++ app/route.js
@@ -4,14 +4,16 @@ const sortArray = require('./sort-array')
 
 const get = async (icao24) => {
   const cacheData = await cache.get('location', icao24)
-  const pathPoints = cacheData.location
+  const path = cacheData.location
     .sort((a, b) => sortArray(a.timestamp, b.timestamp))
-    .map(x => ([x.longitude, x.latitude]))
+  const pathPoints = path.map(x => ([x.longitude, x.latitude]))
   const pathFeature = pathPoints.length > 1 ? turf.lineString(pathPoints) : turf.point(pathPoints[0])
   const locationFeature = turf.point(pathPoints[pathPoints.length - 1])
+  const rotation = path[path.length - 1].trueTrackRadians ?? 0
   return {
     center: [cacheData.location[0]?.source?.longitude ?? 0, cacheData.location[0]?.source?.latitude ?? 0],
-    geoJson: turf.featureCollection([pathFeature, locationFeature])
+    geoJson: turf.featureCollection([pathFeature, locationFeature]),
+    rotation
   }
 }
 
